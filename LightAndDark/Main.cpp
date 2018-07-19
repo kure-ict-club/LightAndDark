94f514522cf8fefdd744a0f4ed28b002e2f361dd
pointN <code.club.nagahashi@gmail.com>
2018-07-19 17:31:25 +0900

    changed title

--- LightAndDark/Main.cpp
+++ LightAndDark/Main.cpp
@@ -8,17 +8,9 @@
 # include "Title.h"
 # include "Game.h"
 
-
-
-
-
-
-
-
-
 void Main()
 {
-	Window::SetTitle(L"ひかりとやみ");
+	Window::SetTitle(L"ひかりとやみ-pointN");
 	Window::SetStyle(WindowStyle::Sizeable);
 
 	SceneManager<BaseSceneName> sceneManager;
